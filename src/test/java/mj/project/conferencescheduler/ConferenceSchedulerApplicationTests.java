876673ba9127f5f0991b0e7ef3a06f7ec23c7b0c
Michal Januszkiewicz <michaljanuszkiewicz6@gmail.com>
2022-06-02 21:02:15 +0200

    Initial commit

---
+++ src/test/java/mj/project/conferencescheduler/ConferenceSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package mj.project.conferencescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
